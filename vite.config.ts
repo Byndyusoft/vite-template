b662f385908453964fd5d84ab86e3aa5c26ce575
Gleb Fomin <gleb.fom28@gmail.com>
2025-01-09 09:52:58 +0500

    feat: add vite-bundle-analyzer for build analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
-/* eslint-disable */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import analyzer from "vite-bundle-analyzer";
+
+const analyzerPlugin = () => (
+  process.env.ANALYZE !== 'true'
+    ? analyzer({ analyzerPort: "auto" })
+    : null
+)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      svgr(),
+      analyzerPlugin(),
+    ].filter(Boolean),
+  };
+});
